9afff2d388c362bb43f7f684312ae30d8474e44a
Rahul-chitturi <Rahulchitturi520@gmail.com>
2020-01-07 19:30:21 +0530

    [Rahul-chitturi] fundoonotes updates added aop and lombok

--- src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
@@ -7,7 +7,6 @@ package com.bridgelabz.fundoonotes.controller;
  * @version :1.0
  */
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.HttpStatus;
@@ -17,9 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bridgelabz.fundoonotes.dto.LoginDetails;
 import com.bridgelabz.fundoonotes.dto.ResetPassword;
 import com.bridgelabz.fundoonotes.dto.UserDto;
@@ -29,7 +28,8 @@ import com.bridgelabz.fundoonotes.response.UserAuthenticationResponse;
 import com.bridgelabz.fundoonotes.service.UserService;
 import com.bridgelabz.fundoonotes.utility.JwtGenerator;
 
-@RestController("users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
 	@Autowired
